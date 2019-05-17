8df23880d2020cb25d4b5d851e934be400e23b7f
agusti-salamero <agusti.salamero@est.fib.upc.edu>
2019-05-17 17:54:51 +0200

    v0.2

---
+++ constants.py
@@ -0,0 +1,12 @@
+# Llistat de constants definides com a variables (lleig pero funcional)
+
+"""
+Esdeveniments acceptats pel simulador
+"""
+EV_ARRIVAL=0
+EV_ENDSERVICE=1
+EV_ARRIVAL_MAINGATE=2
+EV_ENDSERVICE_MAINGATE=3
+EV_ENDSERVICE_PARKING=4
+IDLE=1
+BUSY=2
